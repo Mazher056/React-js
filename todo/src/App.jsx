15b83bc5cfdd32aa2663d9b8863a16581b8efdf4
Mazhar056 <ra103224@gmail.com>
2024-03-05 21:55:31 +0530

    UseReducer added

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,51 +1,59 @@
-import { createContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createContext, useReducer, useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Todoitems from './Components/Todoitems'
 import Displaytodo from './Components/Displaytodo'
 import Addtodo from './Components/Addtodo'
 import Heading from './Components/Heading'
-import { todoitem } from './Store/Todoitem'
+// import  Todoitem  from './Store/Todoitem'
+import Todoitemcontextprovider from './Store/Todoitem'
 
 
 
-export const userCOntext = createContext()
 
 function App() {
-  const arr = [
-    {
-      name: "Good job",
-      date: "22-34-2323"
-    },
-    {
-      name: "Good job1",
-      date: "433-34-23232343"
-    },
-  ]
-  const deletetodo = (name) => {
-    const update = task.filter(e => e.name !== name)
-    settask(update)
-  }
 
-  const addtodo = (name, date) => {
-    const update = [...task, { name, date }]
-    settask(update)
-  }
-  const [task, settask] = useState(arr)
+  
+  //   const reducer = (state,action) => {
+  //     let newarr = state;
+
+  //    if(action.type === "Add"){
+  //     const name =action.payload.name;
+  //     const date =action.payload.date;
+  //      newarr = [...state,{name,date}]
+  //     }
+
+  //     if(action.type === "Delete"){
+  //       newarr = newarr.filter(e=> e.name !== action.payload);
+  //     }
+  //     return newarr;
+
+  //   }
+
+
+  
+  // const [task, dispatch] = useReducer(reducer, []);
+
+  // const deletetodo = (name) => {
+  //   dispatch({type:"Delete",payload:name})
+  // }
+
+  // const addtodo = (name, date) => {
+  //   dispatch({
+  //     type: "Add",
+  //     payload: {
+  //       name, date
+  //     }
+  //   })
+  // }
 
 
   return (
     <>
-      <todoitem.Provider value={{
-        task,
-        addtodo,
-        deletetodo}}>
+      <Todoitemcontextprovider>
         <Heading></Heading>
         <Addtodo></Addtodo>
         <Displaytodo ></Displaytodo>
-      </todoitem.Provider>
+      </Todoitemcontextprovider>
     </>
   )
 }
--- todo/src/Store/Todoitem.jsx
+++ todo/src/Store/Todoitem.jsx
@@ -1,3 +1,52 @@
-import React, { createContext } from 'react'
+import React, { Children, useReducer, createContext } from 'react'
 
-export const todoitem  = createContext(); 
+
+export const todoitem = createContext({
+    task: [],
+    addtodo: () => { },
+    deletetodo: () => { }
+});
+
+
+export default function Todoitemcontextprovider({ children }) {
+
+
+    const reducer = (state, action) => {
+        let newarr = state;
+
+        if (action.type === "Add") {
+            const name = action.payload.name;
+            const date = action.payload.date;
+            newarr = [...state, { name, date }]
+        }
+
+        if (action.type === "Delete") {
+            newarr = newarr.filter(e => e.name !== action.payload);
+        }
+        return newarr;
+
+    }
+
+    const [task, dispatch] = useReducer(reducer, []);
+
+    const deletetodo = (name) => {
+        dispatch({ type: "Delete", payload: name })
+    }
+
+    const addtodo = (name, date) => {
+        dispatch({
+            type: "Add",
+            payload: {
+                name, date
+            }
+        })
+    }
+
+    return (
+
+        <todoitem.Provider value={{ task, addtodo, deletetodo }}>
+            {children}
+        </todoitem.Provider>
+
+    )
+}
